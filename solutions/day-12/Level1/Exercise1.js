8a3622d34dd094e50f0f17bbcb961b69a8c6ec92
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-20 09:03:37 -0500

    Solve question of the Level1 in days 9, 11 and 12

---
+++ solutions/day-12/Level1/Exercise1.js
@@ -0,0 +1,47 @@
+/*
+How many input types do you know?
+text, password, checkbox, radio, submit, reset, button, image, file, hidden, 
+date, datetime-local, email, month, number, range, search, tel, time, url, week
+
+Mention at least four attributes of an input element.
+type, name, value, checked, disabled, readonly, size, maxlength, minlength,
+
+What is the importance of htmlFor attribute?
+It is used to bind a label to an input element.
+
+Write an input type which is not given in the example if there is any.
+color, datetime, datetime-local, email, month, number, range, search, tel, time, url, week
+
+What is a controlled input?
+A controlled input is an input whose value is controlled by React.
+
+What do you need to write a controlled input?
+You need to write an event handler which updates the state when the value of the input changes.
+
+What event type do you use to listen changes on an input field?
+onChange
+
+What is the value of a checked checkbox?
+true or false
+
+When do you use onChange, onBlur and onSubmit?
+onChange is used to listen to changes on an input field.
+onBlur is used to listen to when an input field loses focus.
+onSubmit is used to listen to when a form is submitted.
+
+What is the purpose of writing e.prventDefault() inside the submit event handler?
+It prevents the default action of the submit event.
+
+How do you bind data in React? The first input field example is data binding in React.
+We bind data in React by using the value attribute of the input element.
+
+What is validation in React?
+Validation in React is the process of checking if the data entered by the user is valid.
+
+What is the event type you use to listen when an input changes?
+onChange
+
+What are event types do you use to validate an input?
+onBlur, onChange
+
+*/
