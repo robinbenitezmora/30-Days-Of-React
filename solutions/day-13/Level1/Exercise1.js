a311c7ffe1a6ed3feb4587e4eb064476553d6b03
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-21 08:52:46 -0500

    Solve Level1 questions in day 13

---
+++ solutions/day-13/Level1/Exercise1.js
@@ -0,0 +1,32 @@
+/*
+What is a controlled input?
+A controlled input is an input that is controlled by the state of the component.
+
+What is an uncontrolled input?
+An uncontrolled input is an input that is not controlled by the state of the component.
+
+How do you get a content of a certain HTML element in React?
+You can use the ref attribute to get a reference to the element.
+
+Why it is not a good idea to touch the DOM directly?
+It is not a good idea to touch the DOM directly because 
+it can cause performance issues and it can break the React's virtual DOM.
+
+What is the most frequently used in React? Controlled or uncontrolled inputs?
+The most frequently used in React is the controlled input.
+
+What do you need to write uncontroled inputs?
+You need to use the ref attribute to get a reference to the element.
+
+Does state require to write uncontrolled inputs?
+No, state is not required to write uncontrolled inputs.
+
+When do you use uncontrolled inputs?
+You use uncontrolled inputs when you need to get the value of an input
+
+When do you use controlled inputs?
+You use controlled inputs when you need to control the value of an input
+
+Do you use controlled or uncontrolled inputs to validate the input?
+You use controlled inputs to validate the input.
+*/
